986701dc944e24a87dd87764f7b1b705bcaafbe4
qiqzhao <bme_ritter@foxmail.com>
2024-08-23 12:00:47 +0800

    feat: storybook

---
+++ src/stories/Button.stories.js
@@ -0,0 +1,42 @@
+import { fn } from "@storybook/test";
+import { Button } from "../index";
+
+export default {
+  title: "Components/Button",
+  component: Button,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    backgroundColor: { control: "color" },
+  },
+  args: { onClick: fn() },
+};
+
+export const Primary = {
+  args: {
+    primary: true,
+    label: "Button",
+  },
+};
+
+export const Secondary = {
+  args: {
+    label: "Button",
+  },
+};
+
+export const Large = {
+  args: {
+    size: "large",
+    label: "Button",
+  },
+};
+
+export const Small = {
+  args: {
+    size: "small",
+    label: "Button",
+  },
+};
