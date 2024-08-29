ad3e0c324d408d01434fbb3e4315a8d983f6cd11
qiqzhao <bme_ritter@foxmail.com>
2024-08-29 15:04:43 +0800

    feat: move changelog position

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,6 +6,7 @@ const config = {
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
+    "@storybook/addon-docs",
   ],
   core: {
     builder: "@storybook/builder-vite",
